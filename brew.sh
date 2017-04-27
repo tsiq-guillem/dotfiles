50784e2525a9ca2aa79b1e046834567905d1c29b
Guillem Palou <guillem@guillem-mbp.local>
2017-04-27 10:06:21 +0100

    Improving dotfiles

--- brew.sh
+++ brew.sh
@@ -2,13 +2,21 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew cask install spectacle
+brew cask install iterm
 brew cask install firefox
+brew cask install spotify
 brew cask install vlc
 brew cask install flux
 brew cask install macvim
-brew cask install pycharm
-brew cask install intellij-idea
+brew cask install slack
+brew cask install pycharm-ce
+brew cask install intellij-idea-ce
+brew cask install dropbox
+brew cask install keeweb
 
+brew install wget
+brew install tmux
 brew install youtube-dl
 brew install 7xzip
 brew install neovim/neovim/neovim
--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+mkdir ~/.config
+ln -sv $SCRIPT_PATH/vim/vimrc ~/.vimrc
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+git clone git@github.com:tstirrat/hammerspoon-config.git ~/.hammerspoon
+cd ~/.hammerspoon
+cp config.example.lua config.lua
+
